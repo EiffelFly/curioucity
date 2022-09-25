e9468f8dbf4ab9a29f42b17aa32feb2848e0f2f8
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-09-25 09:21:53 +0800

    feat: add the foundation of discord bot

---
+++ packages/discord-bot/src/main.rs
@@ -0,0 +1,68 @@
+use std::env;
+
+use serenity::async_trait;
+use serenity::model::{channel::Message, gateway::Ready};
+use serenity::prelude::*;
+
+const HELP_MESSAGE: &str = "
+Hello there, Human!
+
+You have summoned me. Let's see about getting you what you need.
+
+❓ Need technical help?
+➡️ Post in the <#1023393389313527888> channel and other humans will assist you.
+
+❓ Looking for the Code of Conduct?
+➡️ Here it is: <https://opensource.facebook.com/code-of-conduct>
+
+❓ Something wrong?
+➡️ You can flag an admin with @admin
+
+I hope that resolves your issue!
+— HelpBot 🤖
+";
+
+const HELP_COMMAND: &str = "!curious";
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == HELP_COMMAND {
+            println!("hi, it's working");
+
+            if let Err(why) = msg.channel_id.say(&ctx.http, HELP_MESSAGE).await {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    // Configure the client with your Discord bot token in the environment.
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    // Build our client.
+    let mut client = Client::builder(token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("Error creating client");
+
+    // Finally, start a single shard, and start listening to events.
+    //
+    // Shards will automatically attempt to reconnect, and will perform
+    // exponential backoff until it reconnects.
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
+}
