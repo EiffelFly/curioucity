2d0ba8be6d9ef47328b43700606e6ebeb50cd2ab
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-09-29 10:26:18 +0800

    feat: get thread messages

--- packages/discord-bot/src/commands/curious_save.rs
+++ packages/discord-bot/src/commands/curious_save.rs
@@ -2,12 +2,10 @@ use std::env;
 
 use serenity::builder::CreateApplicationCommand;
 use serenity::http::HttpBuilder;
-use serenity::model::id::MessageId;
-use serenity::model::prelude::command::{CommandOptionType, CommandType};
-use serenity::model::prelude::interaction::application_command::{
-    CommandDataOption, CommandDataOptionValue,
-};
-use serenity::model::prelude::ChannelId;
+use serenity::model::channel::ChannelType;
+use serenity::model::prelude::command::CommandType;
+use serenity::model::prelude::interaction::application_command::CommandDataOption;
+use serenity::model::prelude::{ChannelId, MessageId};
 
 pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
     command
@@ -16,40 +14,58 @@ pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicatio
         .description(
             "To save specific thread you need to input the message id that open the thread",
         )
-        .create_option(|option| {
-            option
-                .name("thread-id")
-                .description("The message ID that open the thread")
-                .kind(CommandOptionType::String)
-                .required(true)
-        })
 }
 
-pub async fn run(options: &[CommandDataOption], channel_id: ChannelId) -> String {
-    let option = options
-        .get(0)
-        .expect("Expected to have the message id")
-        .resolved
-        .as_ref()
-        .expect("Should input thread ID");
+pub async fn run(_options: &[CommandDataOption], channel_id: ChannelId) -> String {
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    let http = HttpBuilder::new(token).ratelimiter_disabled(true).build();
+    let channel_res = http.get_channel(*channel_id.as_u64()).await;
 
-    println!("Option: {:#?}", option);
-    println!("Channel: {:#?}", channel_id);
-
-    if let CommandDataOptionValue::String(user_input) = option {
-        match user_input.parse::<u64>() {
-            Ok(message_id_num) => {
-                let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
-                let message_id = MessageId(message_id_num);
-                let http = HttpBuilder::new(token).ratelimiter_disabled(true).build();
-                let messages = channel_id
-                    .messages(&http, |retriever| retriever.after(message_id).limit(25))
-                    .await;
-                println!("Messages: {:#?}", messages);
+    match channel_res {
+        Ok(channel) => {
+            let guild = channel.guild().unwrap();
+            let kind = guild.kind;
+            let last_message_id = guild.last_message_id.unwrap();
+            match kind {
+                ChannelType::NewsThread => {
+                    return save_thread_message(channel_id, last_message_id).await
+                }
+                ChannelType::PrivateThread => {
+                    return save_thread_message(channel_id, last_message_id).await
+                }
+                ChannelType::PublicThread => {
+                    return save_thread_message(channel_id, last_message_id).await
+                }
+                _ => {
+                    return "Opps! You are not in a thread".to_string();
+                }
             }
-            Err(_) => return "The thread id should be number".to_string(),
+        }
+        Err(e) => {
+            println!("Error when retrieve channel info: {:#?}", e);
+            return "Opps! Something went wrong".to_string();
         }
     }
+}
 
-    "Hey, I'm alive!".to_string()
+async fn save_thread_message(channel_id: ChannelId, last_message_id: MessageId) -> String {
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    let http = HttpBuilder::new(token).ratelimiter_disabled(true).build();
+    let messages_res = channel_id
+        .messages(&http, |retriever| {
+            retriever.before(last_message_id).limit(25)
+        })
+        .await;
+
+    match messages_res {
+        Ok(messages) => {
+            println!("Messages: {:#?}", messages);
+            return "✨ We are processing your data, seat tight, it will be a fast journey"
+                .to_string();
+        }
+        Err(e) => {
+            println!("Error when retrieve messages: {:#?}", e);
+            return "Opps! Something went wrong".to_string();
+        }
+    }
 }
