0286e0c8640e912dc4376e801f61d4e6441443d3
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-09-30 10:17:20 +0800

    feat: add the basic axum server config

--- packages/server/src/main.rs
+++ packages/server/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+mod helper;
+
+use axum::{http::StatusCode, response::IntoResponse, routing::post, Json, Router};
+use serde::Deserialize;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    // build our application with a route
+    let app = Router::new().route("/discord/thread", post(create_discord_thread));
+
+    // run our app with hyper
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .with_graceful_shutdown(helper::graceful_shutdown::shutdown_signal())
+        .await
+        .unwrap();
 }
+
+async fn create_discord_thread(Json(payload): Json<CreateDiscordThread>) -> impl IntoResponse {
+    println!("Create thread payload: {:#?}", payload);
+    (StatusCode::CREATED, "")
+}
+
+#[derive(Debug, Deserialize)]
+struct CreateDiscordThread {}
