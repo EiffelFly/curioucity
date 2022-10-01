03a33cdf24366b5a6fc92f1ebd41cc524daf505f
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-10-01 10:50:02 +0800

    feat: add 404 not found fallback

--- packages/server/src/main.rs
+++ packages/server/src/main.rs
@@ -1,5 +1,6 @@
 mod helper;
 
+use axum::handler::Handler;
 use axum::{http::StatusCode, response::IntoResponse, routing::post, Json, Router};
 use serde::Deserialize;
 use std::net::SocketAddr;
@@ -7,15 +8,29 @@ use std::net::SocketAddr;
 #[tokio::main]
 async fn main() {
     // build our application with a route
-    let app = Router::new().route("/discord/thread", post(create_discord_thread));
+    let app = Router::new()
+        .fallback(fallback.into_service())
+        .route("/", axum::routing::get(hello))
+        .route("/discord/thread", post(create_discord_thread));
 
     // run our app with hyper
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8010));
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .with_graceful_shutdown(helper::graceful_shutdown::shutdown_signal())
         .await
         .unwrap();
+}
+
+async fn hello() -> String {
+    "Hello, World!".into()
+}
+
+async fn fallback(uri: axum::http::Uri) -> impl axum::response::IntoResponse {
+    (
+        axum::http::StatusCode::NOT_FOUND,
+        format!("Not found {}", uri),
+    )
 }
 
 async fn create_discord_thread(Json(payload): Json<CreateDiscordThread>) -> impl IntoResponse {
