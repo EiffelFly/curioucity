a148a46153e50579898396b92cd34bf644f31fda
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-10-09 16:03:05 +0800

    feat: setup a pong slash command to test the connection between bot and api
    This commit setup a custom/temp slash command /curious-ping. This command will trigger a request to the API endpoint.
    
    note: I am still using http in the interior request inside the docker, not sure whether this act is secure or not. We need to dip deeper about it.

---
+++ packages/discord-bot/src/commands/curious_ping.rs
@@ -0,0 +1,35 @@
+use std::env;
+
+use serenity::builder::CreateApplicationCommand;
+use serenity::model::prelude::interaction::application_command::CommandDataOption;
+
+pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
+    command.name("curious-ping").description("Test pong")
+}
+
+pub async fn run(_options: &[CommandDataOption]) -> String {
+    let backend_host = env::var("BACKEND_HOST").expect("Expected backend host in the environment");
+    let backend_port = env::var("BACKEND_PORT").expect("Expected backend port in the environment");
+    let client = reqwest::Client::new();
+
+    let resp = client
+        .get(format!(
+            "http://{host}:{port}",
+            host = backend_host,
+            port = backend_port
+        ))
+        .send()
+        .await;
+
+    match resp {
+        Ok(response) => {
+            println!("Response: {:?}", response.text().await);
+        }
+        Err(error) => {
+            println!("Error: {:?}", error);
+            return "Something went wrong".to_string();
+        }
+    }
+
+    "Hey, I'm alive!".to_string()
+}
