2503611721483d453c1bb9681caabe33884fc9e7
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-10-18 10:58:25 +0800

    feat: add insert_or_select_tag fn

--- packages/server/src/db/services/curioucity.rs
+++ packages/server/src/db/services/curioucity.rs
@@ -1,5 +1,5 @@
 pub mod curioucity {
-    use crate::db::source::curioucity::Url;
+    use crate::db::source::curioucity::{Tag, Url};
     use edgedb_tokio::Client;
 
     pub struct InsertOrSelectUrlPayload {
@@ -25,5 +25,29 @@ pub mod curioucity {
             .await?;
 
         Ok(url)
+    }
+
+    pub struct InsertOrSelectTagPayload {
+        name: String,
+    }
+
+    pub async fn insert_or_select_tag(
+        client: Client,
+        payload: &InsertOrSelectTagPayload,
+    ) -> Result<Option<Tag>, anyhow::Error> {
+        let query = "select (
+			insert Tag {
+				name := <str>$0
+			}
+			unless conflict on .name
+      	) {
+			name,
+	  	}";
+
+        let tag = client
+            .query_single::<Tag, (&str,)>(&query, &(&payload.name,))
+            .await?;
+
+        Ok(tag)
     }
 }
