db2cb15c48d755e2fe69aa627232bee9c5748f1b
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-10-23 07:32:48 +0800

    feat: add select_or_insert_tag_to_discord_guild

--- packages/server/src/db/services/discord.rs
+++ packages/server/src/db/services/discord.rs
@@ -1,5 +1,5 @@
 pub mod guild {
-    use crate::db::source::discord::DiscordGuild;
+    use crate::db::source::{curioucity::Tag, discord::DiscordGuild};
     use edgedb_tokio::Client;
     pub struct UpsertDiscordGuildPayload {
         pub guild_id: i64,
@@ -38,6 +38,40 @@ pub mod guild {
             .query_single::<DiscordGuild, (&i64, &str, &str)>(
                 &query,
                 &(&payload.guild_id, &payload.name, &payload.icon),
+            )
+            .await?;
+
+        Ok(guild)
+    }
+
+    pub struct SelectOrInsertTagToDiscordGuildPayload {
+        pub guild_id: i64,
+        pub tag: Tag,
+    }
+
+    pub async fn select_or_insert_tag_to_discord_guild(
+        client: Client,
+        payload: &SelectOrInsertTagToDiscordGuildPayload,
+    ) -> Result<Option<DiscordGuild>, anyhow::Error> {
+        let query = "update DiscordGuild
+            filter
+                .guild_id := <int64>$0
+            set {
+                tags += (
+                    select (
+                        insert Tag {
+                            name := <str>$1
+                        }
+                        unless conflict on .name
+                    )
+                )
+            }
+        ";
+
+        let guild = client
+            .query_single::<DiscordGuild, (&i64, &str)>(
+                &query,
+                &(&payload.guild_id, &payload.tag.name),
             )
             .await?;
 
