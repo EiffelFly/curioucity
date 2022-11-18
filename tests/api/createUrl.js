ea03aaaada6c00aceee9aad86e5ed9d9b09ffd01
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-11-18 10:11:28 +0800

    feat: implement create_url integration test

--- tests/api/createUrl.js
+++ tests/api/createUrl.js
@@ -3,12 +3,39 @@ import { check } from "k6";
 import { API_HOST } from "./rest.js";
 
 export const createUrl = () => {
-  let payload = {
-    url: "https://summerbud.org/id/10",
-    resource_type: "RESOURCE_TYPE_WEBSITE",
+  const url = "https://summerbud.org/id/13";
+
+  let createUrlpayload = {
+    url: url,
+    resourceType: "RESOURCE_TYPE_WEBSITE",
   };
 
-  check(http.request("POST", `${API_HOST}/url`, payload), {
-    "POST /url response status": (r) => r.status === 201,
-  });
+  let headers = {
+    "Content-Type": "application/json",
+  };
+
+  check(
+    http.request("POST", `${API_HOST}/url`, JSON.stringify(createUrlpayload), {
+      headers,
+    }),
+    {
+      "POST /url response status": (r) => r.status === 201,
+    }
+  );
+
+  // let deleteUrlPayload = {
+  //   url,
+  // };
+
+  // check(
+  //   http.request(
+  //     "DELETE",
+  //     `${API_HOST}/url`,
+  //     JSON.stringify(deleteUrlPayload),
+  //     { headers }
+  //   ),
+  //   {
+  //     "DELETE /url response status": (r) => console.log(r),
+  //   }
+  // );
 };
