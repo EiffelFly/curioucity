c577140549437dc905e27bb9bc0503434171a730
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-11-20 08:32:59 +0800

    refactor: rewrite rest_handler to have fine-grained error handling

--- packages/server/src/rest_handler/curioucity.rs
+++ packages/server/src/rest_handler/curioucity.rs
@@ -1,20 +1,50 @@
+use axum::response::Response;
 use axum::{http::StatusCode, response::IntoResponse, Json};
 
 use crate::db::model::curioucity as db_curioucity;
-use crate::helper::error::CurioucityAxumError;
 use crate::pb_gen::curioucity::v1alpha as pb_curioucity;
 
 pub async fn create_url_handler(
     Json(data): Json<pb_curioucity::CreateUrlRequest>,
-) -> Result<impl IntoResponse, CurioucityAxumError> {
-    let client = edgedb_tokio::create_client().await?;
+) -> Result<impl IntoResponse, Response> {
+    let client = match edgedb_tokio::create_client().await {
+        Ok(client) => client,
+        Err(error) => {
+            return Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Something went wrong when access database: {}", error),
+            )
+                .into_response())
+        }
+    };
 
     let payload = db_curioucity::CreateUrlPayload {
         url: data.url,
-        resource_type: pb_curioucity::ResourceType::as_db_type(data.resource_type)?,
+        resource_type: match pb_curioucity::ResourceType::as_db_type(data.resource_type) {
+            Ok(resource_type) => resource_type,
+            Err(error) => {
+                return Err((
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!(
+                        "Something went wrong when transform resource_type: {}",
+                        error
+                    ),
+                )
+                    .into_response())
+            }
+        },
     };
 
-    let url = db_curioucity::Url::create(client, &payload).await?;
+    let url = match db_curioucity::Url::create(client, &payload).await {
+        Ok(url) => url,
+        Err(error) => {
+            return Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Something went wrong when create url: {}", error),
+            )
+                .into_response())
+        }
+    };
 
     let resp = pb_curioucity::CreateUrlResponse {
         url: Some(url.as_pb_type()),
@@ -25,24 +55,61 @@ pub async fn create_url_handler(
 
 pub async fn delete_url_handler(
     Json(data): Json<pb_curioucity::DeleteUrlRequest>,
-) -> Result<impl IntoResponse, CurioucityAxumError> {
-    let client = edgedb_tokio::create_client().await?;
+) -> Result<impl IntoResponse, Response> {
+    let client = match edgedb_tokio::create_client().await {
+        Ok(client) => client,
+        Err(error) => {
+            return Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Something went wrong when access database: {}", error),
+            )
+                .into_response())
+        }
+    };
 
     let payload = db_curioucity::DeleteUrlPayload { url: data.url };
 
-    db_curioucity::Url::delete(client, &payload).await?;
-
-    Ok((StatusCode::NO_CONTENT, ()))
+    match db_curioucity::Url::delete(client, &payload).await {
+        Ok(_) => return Ok((StatusCode::NO_CONTENT, ())),
+        Err(error) => {
+            return Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Something went wrong when delete url: {}", error),
+            )
+                .into_response())
+        }
+    };
 }
 
 pub async fn get_url_handler(
     Json(data): Json<pb_curioucity::GetUrlRequest>,
-) -> Result<impl IntoResponse, CurioucityAxumError> {
-    let client = edgedb_tokio::create_client().await?;
+) -> Result<impl IntoResponse, Response> {
+    let client = match edgedb_tokio::create_client().await {
+        Ok(client) => client,
+        Err(error) => {
+            return Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Something went wrong when access database: {}", error),
+            )
+                .into_response())
+        }
+    };
 
     let payload = db_curioucity::GetUrlPayload { url: data.url };
 
-    let url = db_curioucity::Url::get(client, &payload).await?;
+    let url = match db_curioucity::Url::get(client, &payload).await {
+        Ok(url) => match url {
+            Some(url) => url,
+            None => return Err((StatusCode::NOT_FOUND, "".to_string()).into_response()),
+        },
+        Err(error) => {
+            return Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Something went wrong when get url: {}", error),
+            )
+                .into_response())
+        }
+    };
 
     let resp = pb_curioucity::GetUrlResponse {
         url: Some(url.as_pb_type()),
