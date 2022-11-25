27ecc4ce114455000faa4d419960b6b994543985
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-11-25 10:18:31 +0800

    feat: add deleteUrl api test

--- tests/api/url.js
+++ tests/api/url.js
@@ -2,7 +2,13 @@ import http from "k6/http";
 import { check } from "k6";
 import { API_HOST } from "./rest.js";
 
-export const createUrl = () => {
+export const urlServices = {
+  createUrl,
+  deleteUrl,
+  getUrl,
+};
+
+const createUrl = () => {
   const url = "https://summerbud.org/id/21";
 
   let createUrlPayload = {
@@ -37,6 +43,46 @@ export const createUrl = () => {
     ),
     {
       "createUrl - DELETE /url - response status should be 204": (r) =>
+        r.status === 204,
+    }
+  );
+};
+
+const deleteUrl = () => {
+  const url = "https://summerbud.org/id/test";
+
+  let createUrlPayload = {
+    url: url,
+    resourceType: "RESOURCE_TYPE_WEBSITE",
+  };
+
+  let headers = {
+    "Content-Type": "application/json",
+  };
+
+  check(
+    http.request("POST", `${API_HOST}/url`, JSON.stringify(createUrlPayload), {
+      headers,
+    }),
+    {
+      "deleteUrl - POST /url - response status should be 201": (r) =>
+        r.status === 201,
+    }
+  );
+
+  let deleteUrlPayload = {
+    url,
+  };
+
+  check(
+    http.request(
+      "DELETE",
+      `${API_HOST}/url`,
+      JSON.stringify(deleteUrlPayload),
+      { headers }
+    ),
+    {
+      "deleteUrl - DELETE /url - response status should be 204": (r) =>
         r.status === 204,
     }
   );
