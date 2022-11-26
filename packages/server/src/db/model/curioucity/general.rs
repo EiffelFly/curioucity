d5c486683f716b6ac1cfacd61d29c1590a30eee3
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-11-26 15:21:06 +0800

    feat: implement transform_resource_union_to_pb

--- packages/server/src/db/model/curioucity/general.rs
+++ packages/server/src/db/model/curioucity/general.rs
@@ -1,12 +1,8 @@
 use edgedb_derive::Queryable;
 use serde::{Deserialize, Serialize};
 
+use crate::db::model::third_party as db_third_party;
 use crate::pb_gen::curioucity::v1alpha as pb_curioucity;
-
-use crate::db::model::third_party::{
-    discord::{DiscordGuild, DiscordMessage, DiscordThread},
-    website::Website,
-};
 
 #[derive(Queryable, Serialize, Deserialize, Debug)]
 #[edgedb(json)]
@@ -15,6 +11,17 @@ pub enum ResourceType {
     DiscordThread,
     DiscordMessage,
     Website,
+}
+
+impl From<ResourceType> for String {
+    fn from(value: ResourceType) -> String {
+        match value {
+            ResourceType::Website => "Website".to_string(),
+            ResourceType::DiscordGuild => "DiscordGuild".to_string(),
+            ResourceType::DiscordThread => "DiscordThread".to_string(),
+            ResourceType::DiscordMessage => "DiscordMessage".to_string(),
+        }
+    }
 }
 
 impl ResourceType {
@@ -49,8 +56,45 @@ impl ResourceType {
 #[derive(Queryable, Serialize, Deserialize, Debug)]
 #[edgedb(json)]
 pub enum ResourceUnion {
-    DiscordGuild(DiscordGuild),
-    DiscordThread(DiscordThread),
-    DiscordMessage(DiscordMessage),
-    Website(Website),
+    DiscordGuild(db_third_party::discord::DiscordGuild),
+    DiscordThread(db_third_party::discord::DiscordThread),
+    DiscordMessage(db_third_party::discord::DiscordMessage),
+    Website(db_third_party::website::Website),
+}
+
+impl From<ResourceUnion> for pb_curioucity::ResourceUnion {
+    fn from(value: ResourceUnion) -> Self {
+        transform_resource_union_to_pb(&value)
+    }
+}
+
+impl ResourceUnion {
+    pub fn as_pb_type(&self) -> pb_curioucity::ResourceUnion {
+        transform_resource_union_to_pb(self)
+    }
+}
+
+fn transform_resource_union_to_pb(value: &ResourceUnion) -> pb_curioucity::ResourceUnion {
+    match value {
+        ResourceUnion::DiscordGuild(discord_guild) => pb_curioucity::ResourceUnion {
+            resource: Some(pb_curioucity::resource_union::Resource::DiscordGuild(
+                discord_guild.as_pb_type(),
+            )),
+        },
+        ResourceUnion::DiscordThread(discord_thread) => pb_curioucity::ResourceUnion {
+            resource: Some(pb_curioucity::resource_union::Resource::DiscordThread(
+                discord_thread.as_pb_type(),
+            )),
+        },
+        ResourceUnion::DiscordMessage(discord_message) => pb_curioucity::ResourceUnion {
+            resource: Some(pb_curioucity::resource_union::Resource::DiscordMessage(
+                discord_message.as_pb_type(),
+            )),
+        },
+        ResourceUnion::Website(website) => pb_curioucity::ResourceUnion {
+            resource: Some(pb_curioucity::resource_union::Resource::Website(
+                website.as_pb_type(),
+            )),
+        },
+    }
 }
