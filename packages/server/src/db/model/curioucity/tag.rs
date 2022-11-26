78325f451475a05035c67123c9a0505aac1c5c61
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-11-26 15:38:37 +0800

    feat: implement transform_full_tag_to_pb

--- packages/server/src/db/model/curioucity/tag.rs
+++ packages/server/src/db/model/curioucity/tag.rs
@@ -3,24 +3,18 @@ use edgedb_derive::Queryable;
 use edgedb_tokio::Client;
 use serde::{Deserialize, Serialize};
 
+use crate::pb_gen::curioucity::v1alpha as pb_curioucity;
+
+use super::ResourceUnion;
+
 #[derive(Queryable, Serialize, Deserialize, Debug)]
-pub struct Tag {
+pub struct FullTag {
+    pub id: String,
     pub name: String,
+    pub resources: Option<Vec<ResourceUnion>>,
 }
 
-pub struct CreateTagPayload {
-    name: String,
-}
-
-pub struct DeleteTagPayload {
-    name: String,
-}
-
-pub struct GetTagPayload {
-    name: String,
-}
-
-impl Tag {
+impl FullTag {
     pub async fn create(client: Client, payload: &CreateTagPayload) -> Result<Self, anyhow::Error> {
         let query = "select (
             insert Tag {
@@ -28,14 +22,15 @@ impl Tag {
             }
         ) {
             id,
-            name
+            name,
+            resources
         };";
 
         let response = client.query_json(&query, &(&payload.name,)).await;
 
         match response {
             Ok(json) => {
-                let result = serde_json::from_str::<Vec<Tag>>(json.as_ref()).unwrap();
+                let result = serde_json::from_str::<Vec<FullTag>>(json.as_ref()).unwrap();
                 Ok(result.into_iter().nth(0).unwrap())
             }
             Err(error) => {
@@ -65,14 +60,15 @@ impl Tag {
     ) -> Result<Option<Self>, anyhow::Error> {
         let query = "select Tag {
             id, 
-            name
+            name,
+            resources
         } filter .name = <str>$0";
 
         let response = client.query_json(&query, &(&payload.name,)).await;
 
         match response {
             Ok(json) => {
-                let result = serde_json::from_str::<Vec<Tag>>(json.as_ref()).unwrap();
+                let result = serde_json::from_str::<Vec<FullTag>>(json.as_ref()).unwrap();
 
                 if result.is_empty() {
                     Ok(None)
@@ -87,4 +83,64 @@ impl Tag {
             }
         }
     }
+
+    pub fn as_pb_type(&self) -> pb_curioucity::FullTag {
+        transform_full_tag_to_pb(self)
+    }
+}
+
+fn transform_full_tag_to_pb(value: &FullTag) -> pb_curioucity::FullTag {
+    let mut pb_resources: Vec<pb_curioucity::ResourceUnion> = Vec::new();
+
+    match &value.resources {
+        Some(resources) => {
+            for i in resources {
+                pb_resources.push(i.as_pb_type());
+            }
+        }
+        None => {}
+    }
+
+    return pb_curioucity::FullTag {
+        id: value.id.clone(),
+        name: value.name.clone(),
+        resources: pb_resources,
+    };
+}
+
+#[derive(Queryable, Serialize, Deserialize, Debug)]
+pub struct Tag {
+    pub id: String,
+    pub name: String,
+}
+
+impl Tag {
+    pub fn as_pb_type(&self) -> pb_curioucity::Tag {
+        transform_tag_to_pb(self)
+    }
+}
+
+fn transform_tag_to_pb(value: &Tag) -> pb_curioucity::Tag {
+    pb_curioucity::Tag {
+        id: value.id.clone(),
+        name: value.name.clone(),
+    }
+}
+
+impl From<Tag> for pb_curioucity::Tag {
+    fn from(value: Tag) -> Self {
+        transform_tag_to_pb(&value)
+    }
+}
+
+pub struct CreateTagPayload {
+    pub name: String,
+}
+
+pub struct DeleteTagPayload {
+    pub name: String,
+}
+
+pub struct GetTagPayload {
+    pub name: String,
 }
