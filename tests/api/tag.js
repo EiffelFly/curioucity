0a84efad1bb87c5f31405fdfc23a002b675573b7
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-11-28 09:14:36 +0800

    feat: group e2e test (#12)
    This commit groups e2e test to have better metric.

--- tests/api/tag.js
+++ tests/api/tag.js
@@ -1,159 +1,181 @@
 import http from "k6/http";
-import { check } from "k6";
+import { check, group } from "k6";
 import { API_HOST } from "./rest.js";
 
 export const createTag = () => {
-  const tagName = "knowledge-management-toolkit";
+  group("Tag - Should create tag", () => {
+    const tagName = "knowledge-management-toolkit";
 
-  let createTagPayload = {
-    name: tagName,
-  };
+    let createTagPayload = {
+      name: tagName,
+    };
 
-  let headers = {
-    "Content-Type": "application/json",
-  };
+    let headers = {
+      "Content-Type": "application/json",
+    };
 
-  check(
-    http.request("POST", `${API_HOST}/tag`, JSON.stringify(createTagPayload), {
-      headers,
-    }),
-    {
-      "createTag - POST /tag - response status should be 201": (r) =>
-        r.status === 201,
-      "createTag - POST /tag - response body should have id": (r) =>
-        typeof r.json().tag.id !== undefined || r.json().tag.id !== null,
-      "createTag - POST /tag - response body should have correct tag name": (
-        r
-      ) => r.json().tag.name === tagName,
-    }
-  );
+    check(
+      http.request(
+        "POST",
+        `${API_HOST}/tag`,
+        JSON.stringify(createTagPayload),
+        {
+          headers,
+        }
+      ),
+      {
+        "createTag - POST /tag - response status should be 201": (r) =>
+          r.status === 201,
+        "createTag - POST /tag - response body should have id": (r) =>
+          typeof r.json().tag.id !== undefined || r.json().tag.id !== null,
+        "createTag - POST /tag - response body should have correct tag name": (
+          r
+        ) => r.json().tag.name === tagName,
+      }
+    );
 
-  let deleteTagPayload = {
-    name: tagName,
-  };
+    let deleteTagPayload = {
+      name: tagName,
+    };
 
-  check(
-    http.request(
-      "DELETE",
-      `${API_HOST}/tag`,
-      JSON.stringify(deleteTagPayload),
-      { headers }
-    ),
-    {
-      "createTag - DELETE /tag - response status should be 204": (r) =>
-        r.status === 204,
-    }
-  );
+    check(
+      http.request(
+        "DELETE",
+        `${API_HOST}/tag`,
+        JSON.stringify(deleteTagPayload),
+        { headers }
+      ),
+      {
+        "createTag - DELETE /tag - response status should be 204": (r) =>
+          r.status === 204,
+      }
+    );
+  });
 };
 
 export const deleteTag = () => {
-  const tagName = "knowledge-management-toolkit";
+  group("Tag - Should delete tag", () => {
+    const tagName = "knowledge-management-toolkit";
 
-  let createTagPayload = {
-    name: tagName,
-  };
+    let createTagPayload = {
+      name: tagName,
+    };
 
-  let headers = {
-    "Content-Type": "application/json",
-  };
+    let headers = {
+      "Content-Type": "application/json",
+    };
 
-  check(
-    http.request("POST", `${API_HOST}/tag`, JSON.stringify(createTagPayload), {
-      headers,
-    }),
-    {
-      "deleteTag - POST /tag - response status should be 201": (r) =>
-        r.status === 201,
-    }
-  );
+    check(
+      http.request(
+        "POST",
+        `${API_HOST}/tag`,
+        JSON.stringify(createTagPayload),
+        {
+          headers,
+        }
+      ),
+      {
+        "deleteTag - POST /tag - response status should be 201": (r) =>
+          r.status === 201,
+      }
+    );
 
-  let deleteTagPayload = {
-    name: tagName,
-  };
+    let deleteTagPayload = {
+      name: tagName,
+    };
 
-  check(
-    http.request(
-      "DELETE",
-      `${API_HOST}/tag`,
-      JSON.stringify(deleteTagPayload),
-      { headers }
-    ),
-    {
-      "deleteTag - DELETE /tag - response status should be 204": (r) =>
-        r.status === 204,
-    }
-  );
+    check(
+      http.request(
+        "DELETE",
+        `${API_HOST}/tag`,
+        JSON.stringify(deleteTagPayload),
+        { headers }
+      ),
+      {
+        "deleteTag - DELETE /tag - response status should be 204": (r) =>
+          r.status === 204,
+      }
+    );
+  });
 };
 
 export const getTag = () => {
-  // Should return not found when try to get not exist tag
-  const notExistTag = "web3";
+  group("Tag - Should get tag", () => {
+    // Should return not found when try to get not exist tag
+    const notExistTag = "web3";
 
-  let getTagPayload = {
-    name: notExistTag,
-  };
+    let getTagPayload = {
+      name: notExistTag,
+    };
 
-  let headers = {
-    "Content-Type": "application/json",
-  };
+    let headers = {
+      "Content-Type": "application/json",
+    };
 
-  check(
-    http.request("GET", `${API_HOST}/tag`, JSON.stringify(getTagPayload), {
-      headers,
-    }),
-    {
-      "getTag - GET /tag - not exist tag, response status should be 404": (r) =>
-        r.status === 404,
-    }
-  );
+    check(
+      http.request("GET", `${API_HOST}/tag`, JSON.stringify(getTagPayload), {
+        headers,
+      }),
+      {
+        "getTag - GET /tag - not exist tag, response status should be 404": (
+          r
+        ) => r.status === 404,
+      }
+    );
 
-  // Should get the newly created tag
-  const newTagName = "knowledge-management-toolkit";
+    // Should get the newly created tag
+    const newTagName = "knowledge-management-toolkit";
 
-  let createTagPayload = {
-    name: newTagName,
-  };
+    let createTagPayload = {
+      name: newTagName,
+    };
 
-  check(
-    http.request("POST", `${API_HOST}/tag`, JSON.stringify(createTagPayload), {
-      headers,
-    }),
-    {
-      "getTag - POST /tag - response status should be 201": (r) =>
-        r.status === 201,
-    }
-  );
+    check(
+      http.request(
+        "POST",
+        `${API_HOST}/tag`,
+        JSON.stringify(createTagPayload),
+        {
+          headers,
+        }
+      ),
+      {
+        "getTag - POST /tag - response status should be 201": (r) =>
+          r.status === 201,
+      }
+    );
 
-  getTagPayload.name = newTagName;
+    getTagPayload.name = newTagName;
 
-  check(
-    http.request("GET", `${API_HOST}/tag`, JSON.stringify(getTagPayload), {
-      headers,
-    }),
-    {
-      "getTag - GET /tag - response status should be 200": (r) =>
-        r.status === 200,
-      "getTag - GET /tag - response body should have id": (r) =>
-        typeof r.json().tag.id !== undefined || r.json().tag.id !== null,
-      "getTag - GET /tag - response body should have correct tag name": (r) =>
-        r.json().tag.name === newTagName,
-    }
-  );
+    check(
+      http.request("GET", `${API_HOST}/tag`, JSON.stringify(getTagPayload), {
+        headers,
+      }),
+      {
+        "getTag - GET /tag - response status should be 200": (r) =>
+          r.status === 200,
+        "getTag - GET /tag - response body should have id": (r) =>
+          typeof r.json().tag.id !== undefined || r.json().tag.id !== null,
+        "getTag - GET /tag - response body should have correct tag name": (r) =>
+          r.json().tag.name === newTagName,
+      }
+    );
 
-  let deleteTagPayload = {
-    name: newTagName,
-  };
+    let deleteTagPayload = {
+      name: newTagName,
+    };
 
-  check(
-    http.request(
-      "DELETE",
-      `${API_HOST}/tag`,
-      JSON.stringify(deleteTagPayload),
-      { headers }
-    ),
-    {
-      "getTag - DELETE /tag - response status should be 204": (r) =>
-        r.status === 204,
-    }
-  );
+    check(
+      http.request(
+        "DELETE",
+        `${API_HOST}/tag`,
+        JSON.stringify(deleteTagPayload),
+        { headers }
+      ),
+      {
+        "getTag - DELETE /tag - response status should be 204": (r) =>
+          r.status === 204,
+      }
+    );
+  });
 };
--- tests/api/url.js
+++ tests/api/url.js
@@ -1,166 +1,188 @@
 import http from "k6/http";
-import { check } from "k6";
+import { check, group } from "k6";
 import { API_HOST } from "./rest.js";
 
 export const createUrl = () => {
-  const url = "https://summerbud.org/id/21";
+  group("Url - Should create url", () => {
+    const url = "https://summerbud.org/id/21";
 
-  let createUrlPayload = {
-    url: url,
-    resource_type: "RESOURCE_TYPE_WEBSITE",
-  };
+    let createUrlPayload = {
+      url: url,
+      resource_type: "RESOURCE_TYPE_WEBSITE",
+    };
 
-  let headers = {
-    "Content-Type": "application/json",
-  };
+    let headers = {
+      "Content-Type": "application/json",
+    };
 
-  check(
-    http.request("POST", `${API_HOST}/url`, JSON.stringify(createUrlPayload), {
-      headers,
-    }),
-    {
-      "createUrl - POST /url - response status should be 201": (r) =>
-        r.status === 201,
-      "createUrl - POST /url - response body should have id": (r) =>
-        typeof r.json().url.id !== undefined || r.json().url.id !== null,
-      "createUrl - POST /url - response body should have correct url": (r) =>
-        r.json().url.url === url,
-      "createUrl - POST /url - response body should have correct resource_type":
-        (r) => r.json().url.resource_type === createUrlPayload.resource_type,
-    }
-  );
+    check(
+      http.request(
+        "POST",
+        `${API_HOST}/url`,
+        JSON.stringify(createUrlPayload),
+        {
+          headers,
+        }
+      ),
+      {
+        "createUrl - POST /url - response status should be 201": (r) =>
+          r.status === 201,
+        "createUrl - POST /url - response body should have id": (r) =>
+          typeof r.json().url.id !== undefined || r.json().url.id !== null,
+        "createUrl - POST /url - response body should have correct url": (r) =>
+          r.json().url.url === url,
+        "createUrl - POST /url - response body should have correct resource_type":
+          (r) => r.json().url.resource_type === createUrlPayload.resource_type,
+      }
+    );
 
-  let deleteUrlPayload = {
-    url,
-  };
+    let deleteUrlPayload = {
+      url,
+    };
 
-  check(
-    http.request(
-      "DELETE",
-      `${API_HOST}/url`,
-      JSON.stringify(deleteUrlPayload),
-      { headers }
-    ),
-    {
-      "createUrl - DELETE /url - response status should be 204": (r) =>
-        r.status === 204,
-    }
-  );
+    check(
+      http.request(
+        "DELETE",
+        `${API_HOST}/url`,
+        JSON.stringify(deleteUrlPayload),
+        { headers }
+      ),
+      {
+        "createUrl - DELETE /url - response status should be 204": (r) =>
+          r.status === 204,
+      }
+    );
+  });
 };
 
 export const deleteUrl = () => {
-  const url = "https://summerbud.org/id/test";
+  group("Url - Should delete url", () => {
+    const url = "https://summerbud.org/id/test";
 
-  let createUrlPayload = {
-    url: url,
-    resourceType: "RESOURCE_TYPE_WEBSITE",
-  };
+    let createUrlPayload = {
+      url: url,
+      resourceType: "RESOURCE_TYPE_WEBSITE",
+    };
 
-  let headers = {
-    "Content-Type": "application/json",
-  };
+    let headers = {
+      "Content-Type": "application/json",
+    };
 
-  check(
-    http.request("POST", `${API_HOST}/url`, JSON.stringify(createUrlPayload), {
-      headers,
-    }),
-    {
-      "deleteUrl - POST /url - response status should be 201": (r) =>
-        r.status === 201,
-    }
-  );
+    check(
+      http.request(
+        "POST",
+        `${API_HOST}/url`,
+        JSON.stringify(createUrlPayload),
+        {
+          headers,
+        }
+      ),
+      {
+        "deleteUrl - POST /url - response status should be 201": (r) =>
+          r.status === 201,
+      }
+    );
 
-  let deleteUrlPayload = {
-    url,
-  };
+    let deleteUrlPayload = {
+      url,
+    };
 
-  check(
-    http.request(
-      "DELETE",
-      `${API_HOST}/url`,
-      JSON.stringify(deleteUrlPayload),
-      { headers }
-    ),
-    {
-      "deleteUrl - DELETE /url - response status should be 204": (r) =>
-        r.status === 204,
-    }
-  );
+    check(
+      http.request(
+        "DELETE",
+        `${API_HOST}/url`,
+        JSON.stringify(deleteUrlPayload),
+        { headers }
+      ),
+      {
+        "deleteUrl - DELETE /url - response status should be 204": (r) =>
+          r.status === 204,
+      }
+    );
+  });
 };
 
 export const getUrl = () => {
-  // Should return not found when try to get not exist url
-  const notExistUrl = "https://summerbud.org/id/21afr23";
+  group("Url - Should get url", () => {
+    // Should return not found when try to get not exist url
+    const notExistUrl = "https://summerbud.org/id/21afr23";
 
-  let getUrlPayload = {
-    url: notExistUrl,
-  };
+    let getUrlPayload = {
+      url: notExistUrl,
+    };
 
-  let headers = {
-    "Content-Type": "application/json",
-  };
+    let headers = {
+      "Content-Type": "application/json",
+    };
 
-  check(
-    http.request("GET", `${API_HOST}/url`, JSON.stringify(getUrlPayload), {
-      headers,
-    }),
-    {
-      "getUrl - GET /url - not exist url, response status should be 404": (r) =>
-        r.status === 404,
-    }
-  );
+    check(
+      http.request("GET", `${API_HOST}/url`, JSON.stringify(getUrlPayload), {
+        headers,
+      }),
+      {
+        "getUrl - GET /url - not exist url, response status should be 404": (
+          r
+        ) => r.status === 404,
+      }
+    );
 
-  // Should get the newly created url
-  const newUrl = "https://summerbud.org/id/333423452";
+    // Should get the newly created url
+    const newUrl = "https://summerbud.org/id/333423452";
 
-  let createUrlPayload = {
-    url: newUrl,
-    resource_type: "RESOURCE_TYPE_WEBSITE",
-  };
+    let createUrlPayload = {
+      url: newUrl,
+      resource_type: "RESOURCE_TYPE_WEBSITE",
+    };
 
-  check(
-    http.request("POST", `${API_HOST}/url`, JSON.stringify(createUrlPayload), {
-      headers,
-    }),
-    {
-      "getUrl - POST /url - response status should be 201": (r) =>
-        r.status === 201,
-    }
-  );
+    check(
+      http.request(
+        "POST",
+        `${API_HOST}/url`,
+        JSON.stringify(createUrlPayload),
+        {
+          headers,
+        }
+      ),
+      {
+        "getUrl - POST /url - response status should be 201": (r) =>
+          r.status === 201,
+      }
+    );
 
-  getUrlPayload.url = newUrl;
+    getUrlPayload.url = newUrl;
 
-  check(
-    http.request("GET", `${API_HOST}/url`, JSON.stringify(getUrlPayload), {
-      headers,
-    }),
-    {
-      "getUrl - GET /url - response status should be 200": (r) =>
-        r.status === 200,
-      "getUrl - GET /url - response body should have id": (r) =>
-        typeof r.json().url.id !== undefined || r.json().url.id !== null,
-      "getUrl - GET /url - response body should have correct url": (r) =>
-        r.json().url.url === newUrl,
-      "getUrl - GET /url - response body should have correct resource_type": (
-        r
-      ) => r.json().url.resource_type === createUrlPayload.resource_type,
-    }
-  );
+    check(
+      http.request("GET", `${API_HOST}/url`, JSON.stringify(getUrlPayload), {
+        headers,
+      }),
+      {
+        "getUrl - GET /url - response status should be 200": (r) =>
+          r.status === 200,
+        "getUrl - GET /url - response body should have id": (r) =>
+          typeof r.json().url.id !== undefined || r.json().url.id !== null,
+        "getUrl - GET /url - response body should have correct url": (r) =>
+          r.json().url.url === newUrl,
+        "getUrl - GET /url - response body should have correct resource_type": (
+          r
+        ) => r.json().url.resource_type === createUrlPayload.resource_type,
+      }
+    );
 
-  let deleteUrlPayload = {
-    url: newUrl,
-  };
+    let deleteUrlPayload = {
+      url: newUrl,
+    };
 
-  check(
-    http.request(
-      "DELETE",
-      `${API_HOST}/url`,
-      JSON.stringify(deleteUrlPayload),
-      { headers }
-    ),
-    {
-      "getUrl - DELETE /url - response status should be 204": (r) =>
-        r.status === 204,
-    }
-  );
+    check(
+      http.request(
+        "DELETE",
+        `${API_HOST}/url`,
+        JSON.stringify(deleteUrlPayload),
+        { headers }
+      ),
+      {
+        "getUrl - DELETE /url - response status should be 204": (r) =>
+          r.status === 204,
+      }
+    );
+  });
 };
