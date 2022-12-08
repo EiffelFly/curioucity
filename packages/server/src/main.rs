3483372268da3b07ff4c5ba346d0f3e239e9dae5
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-12-08 10:36:39 +0800

    feat: add discord_services into main

--- packages/server/src/main.rs
+++ packages/server/src/main.rs
@@ -11,12 +11,16 @@ use std::net::SocketAddr;
 
 use axum::Router;
 use grpc_handler::curioucity::{GrpcTagServiceImpl, GrpcUrlServiceImpl};
+use grpc_handler::third_party::discord::GrpcDiscordServiceImpl;
 use http::{header::CONTENT_TYPE, Request};
 use hyper::Body;
 use pb_gen::curioucity::v1alpha as pb_curioucity;
+use pb_gen::third_party::v1alpha as pb_third_party;
 use rest_handler::curioucity::{
     create_tag, create_url, delete_tag, delete_url, get_tag, get_url, list_tag, list_url,
 };
+use rest_handler::third_party::discord::create_discord_message;
+
 use tonic::transport::Server;
 use tower::{steer::Steer, BoxError, ServiceExt};
 use tracing_subscriber;
@@ -36,6 +40,10 @@ async fn main() {
         .route("/tags", axum::routing::delete(delete_tag))
         .route("/tags/:name", axum::routing::get(get_tag))
         .route("/tags", axum::routing::get(list_tag))
+        .route(
+            "/discord/messages",
+            axum::routing::post(create_discord_message),
+        )
         .fallback(fallback)
         .map_response(|r| r.map(axum::body::boxed))
         .map_err(BoxError::from)
@@ -47,9 +55,14 @@ async fn main() {
     let tag_service =
         pb_curioucity::tag_service_server::TagServiceServer::new(GrpcTagServiceImpl::default());
 
+    let discord_service = pb_third_party::discord_service_server::DiscordServiceServer::new(
+        GrpcDiscordServiceImpl::default(),
+    );
+
     let grpc = Server::builder()
         .add_service(url_service)
         .add_service(tag_service)
+        .add_service(discord_service)
         .into_service()
         .map_response(|r| r.map(axum::body::boxed))
         .map_err(BoxError::from)
