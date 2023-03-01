eee477d3d626050f1fb575f22928c59585537619
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-03-01 10:41:54 +0800

    feat: add Add gRPC test of CreateTag service (#47)
    This PR adds gRPC test of CreateTag service

--- tests/api/grpc.js
+++ tests/api/grpc.js
@@ -1,12 +1,14 @@
 import * as urlServices from "./grpc_url.js";
+import * as tagServices from "./grpc_tag.js";
 
 export const GRPC_API_HOST = "localhost:8080";
 
 const grpc = () => {
-  urlServices.createUrl();
-  urlServices.deleteUrl();
-  urlServices.getUrl();
-  urlServices.listUrl();
+  // urlServices.createUrl();
+  // urlServices.deleteUrl();
+  // urlServices.getUrl();
+  // urlServices.listUrl();
+  tagServices.createTag();
 };
 
 export default grpc;
