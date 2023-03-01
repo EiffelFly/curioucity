26c7d5adacd49edd7eaa5f50b411485a7b3b66c8
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-03-01 10:54:47 +0800

    feat: add gRPC test of GetTag service (#49)

--- tests/api/grpc_tag.js
+++ tests/api/grpc_tag.js
@@ -82,3 +82,64 @@ export const deleteTag = () => {
     });
   });
 };
+
+export const getTag = () => {
+  group("gRPC TagService - Should get tag", () => {
+    client.connect(GRPC_API_HOST, { timeout: 2000, plaintext: true });
+
+    const getNotExistTagResponse = client.invoke(
+      "curioucity.v1alpha.TagService/GetTag",
+      { name: makeRandStr(6) }
+    );
+
+    check(getNotExistTagResponse, {
+      "GetTag - not exist tag, response status should be StatusNotFound": (r) =>
+        r.status === grpc.StatusNotFound,
+    });
+
+    const createTagPayload = {
+      name: makeRandStr(6),
+    };
+
+    const createTagResponse = client.invoke(
+      "curioucity.v1alpha.TagService/CreateTag",
+      createTagPayload
+    );
+
+    check(createTagResponse, {
+      "GetTag - create test tag - response status should be StatusOK": (r) =>
+        r.status === grpc.StatusOK,
+    });
+
+    const getNewTagResponse = client.invoke(
+      "curioucity.v1alpha.TagService/GetTag",
+      { name: createTagPayload.name }
+    );
+
+    check(getNewTagResponse, {
+      "GetTag - response status should be StatusOK": (r) =>
+        r.status === grpc.StatusOK,
+      "GetTag - response body should have id": (r) =>
+        typeof r.message.tag.id !== undefined || r.message.tag.id !== null,
+      "GetTag - response body should have correct tag name": (r) =>
+        r.message.tag.name === createTagPayload.name,
+      "GetTag - response body should have created_timestamp_at_curioucity": (
+        r
+      ) =>
+        r.message.tag.createdTimestampAtCurioucity !== null ||
+        r.message.tag.createdTimestampAtCurioucity !== undefined,
+    });
+
+    const deleteTagResponse = client.invoke(
+      "curioucity.v1alpha.TagService/DeleteTag",
+      {
+        name: createTagPayload.name,
+      }
+    );
+
+    check(deleteTagResponse, {
+      "GetTag - response status should be StatusOK": (r) =>
+        r.status === grpc.StatusOK,
+    });
+  });
+};
