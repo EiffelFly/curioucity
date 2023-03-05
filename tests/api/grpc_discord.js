de4f671369bab2c92b21a9c17496d2beec591e81
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-03-05 11:14:48 +0800

    feat: Add gRPC test for getting the discord guild (#52)

--- tests/api/grpc_discord.js
+++ tests/api/grpc_discord.js
@@ -102,3 +102,83 @@ export const deleteDiscordGuild = () => {
     });
   });
 };
+
+export const getDiscordGuild = () => {
+  group("gRPC DiscordService - Should get discord guild", () => {
+    const getNotExistDiscordGuildResponse = client.invoke(
+      "third_party.v1alpha.DiscordService/GetDiscordGuild",
+      {
+        guild_id: `${Math.floor(Math.random() * 100000000)}`,
+      }
+    );
+
+    check(getNotExistDiscordGuildResponse, {
+      "GetDiscordGuild - get not exist discord guild, response status should be StatusNotFound":
+        (r) => r.status === grpc.StatusNotFound,
+    });
+
+    const createDiscordGuildPayload = {
+      guild_id: `${Math.floor(Math.random() * 100000000)}`,
+      icon: "icon",
+      name: "Curioucity",
+      url: `https://discord.com/id/${Math.floor(Math.random() * 100000000)}`,
+      created_timestamp_at_discord: 1675220675,
+    };
+
+    const createDiscordGuildResponse = client.invoke(
+      "third_party.v1alpha.DiscordService/CreateDiscordGuild",
+      createDiscordGuildPayload
+    );
+
+    check(createDiscordGuildResponse, {
+      "GetDiscordGuild - create test discord guild - response status should be StatusOK":
+        (r) => r.status === grpc.StatusOK,
+    });
+
+    const getExistDiscordGuildResponse = client.invoke(
+      "third_party.v1alpha.DiscordService/GetDiscordGuild",
+      {
+        guild_id: createDiscordGuildPayload.guild_id,
+      }
+    );
+
+    check(getExistDiscordGuildResponse, {
+      "GetDiscordGuild  - response status should be StatusOK": (r) =>
+        r.status === grpc.StatusOK,
+      "GetDiscordGuild - response body should have id": (r) =>
+        typeof r.message.discordGuild.id !== undefined &&
+        r.message.discordGuild.id !== null,
+      "GetDiscordGuild - response body should have correct guild_id": (r) =>
+        r.message.discordGuild.guildId ===
+        createDiscordGuildPayload.guild_id.toString(),
+      "GetDiscordGuild - response body should have correct icon": (r) =>
+        r.message.discordGuild.icon === createDiscordGuildPayload.icon,
+      "GetDiscordGuild - response body should have correct name": (r) =>
+        r.message.discordGuild.name === createDiscordGuildPayload.name,
+      "GetDiscordGuild - response body should have correct url": (r) =>
+        r.message.discordGuild.url.url === createDiscordGuildPayload.url,
+      "GetDiscordGuild - response body should have correct created_timestamp_at_discord":
+        (r) =>
+          Date.parse(r.message.discordGuild.createdTimestampAtDiscord) /
+            1000 ===
+          createDiscordGuildPayload.created_timestamp_at_discord,
+      "GetDiscordGuild - response body should have created_timestamp_at_curioucity":
+        (r) =>
+          typeof r.message.discordGuild.createdTimestampAtDiscord !==
+            "undefined" &&
+          r.message.discordGuild.createdTimestampAtDiscord !== null,
+    });
+
+    const deleteDiscordGuildResponse = client.invoke(
+      "third_party.v1alpha.DiscordService/DeleteDiscordGuild",
+      {
+        guild_id: createDiscordGuildPayload.guild_id,
+      }
+    );
+
+    check(deleteDiscordGuildResponse, {
+      "DeleteDiscordGuild - delete discord guild - response status should be StatusOK":
+        (r) => r.status === grpc.StatusOK,
+    });
+  });
+};
